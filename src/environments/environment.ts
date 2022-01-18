808e46990682ea724114e5ff1f9f604d9363beba
Sidnasciment0 <sid.nas@outlook.com>
2022-01-18 14:42:28 -0300

    Criação do Firebase.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-61e9a',
+    appId: '1:536032776514:web:418ed09fd3ed898bc838db',
+    storageBucket: 'filmflix-61e9a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAazPEd8ZB2HoNx3_W_9mTD9-hlT4ieggA',
+    authDomain: 'filmflix-61e9a.firebaseapp.com',
+    messagingSenderId: '536032776514',
+  },
   production: false
 };
 
